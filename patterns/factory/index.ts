9f35311bc83842ea23b76a1151e982335b9957f5
Dipesh Patil <dipeshpatil@Ds-MacBook-Pro.local>
2023-04-17 00:07:41 +0530

    Minor Code Documentation Changes

--- patterns/factory/index.ts
+++ patterns/factory/index.ts
@@ -5,8 +5,10 @@ import { CoinType } from "./coin_type";
 const copperCoin = CoinFactory.getCoin(CoinType.CopperCoin);
 console.log(copperCoin.getDescription());
 
+// Creating a silver coin
 const silverCoin = CoinFactory.getCoin(CoinType.SilverCoin);
 console.log(silverCoin.getDescription());
 
+// Creating a gold coin
 const goldCoin = CoinFactory.getCoin(CoinType.GoldCoin);
 console.log(goldCoin.getDescription());
